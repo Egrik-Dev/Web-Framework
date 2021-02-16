f5c1c2b85be82c9052ecaec6534f6e5aa72623d8
Egrik <egor-office-mail@yandex.ru>
2021-02-16 22:56:41 +0300

    Добавляет все файлы на проект

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { User } from "./models/User";
+
+const userEgor = new User({ name: `Fedor`, age: 32 });
+console.log(`Work`);
---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
+
+export interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+const rootUrl = `http://localhost:3000/users`;
+
+export class User {
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+}
+
+// .then((response: AxiosResponse): void => {
+//   this.set(response.data);
+// });
