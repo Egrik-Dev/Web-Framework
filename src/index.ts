0e00fb6b70ec819581423dd3e3d3180f56ed5a3b
Egrik <egor-office-mail@yandex.ru>
2021-03-01 22:25:44 +0300

    Реализует работу view классов - Добавлен tsconfig для точной проверки типов - в index.ts добавлена проверка на наличие корневого элемента - Добавлен метод задающий рандомный возраст юзера - Добавлен корневой класс UserEdit для отрисовки дочерних view - В классе Формы добавлены методы для биндинга событий кнопок - Логика отрисовки перенесена в абстрактный класс View - Реализован класс UserShow для отрисовки данных пользователя

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
 
 const root = document.querySelector(`#root`);
 
 const user = User.buildUser({ name: `Ben`, age: 22 });
-const userForm = new UserForm(root, user);
 
-userForm.render();
+if (root) {
+  const userEdit = new UserEdit(root, user);
+
+  userEdit.render();
+  console.log(userEdit);
+} else {
+  throw new Error("Root not found");
+}
